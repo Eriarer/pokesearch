ce6697c7ae7089ae038c7ca06241f2e00070e905
Laxlev <leofrivgames1@gmail.com>
2024-03-07 22:58:32 -0600

    Refactor image selection and dropping logic

--- Game/js/game.js
+++ Game/js/game.js
@@ -1,24 +1,19 @@
  // Array que contiene las rutas de tus imágenes
  var imagenes = [
-    { pokemon: 'media/001.png', fondo: 'media/001.png' },
-    { pokemon: 'media/007.png', fondo: 'media/007.png' },
-    { pokemon: 'media/024.png', fondo: 'media/024.png' },
-    { pokemon: 'media/025.png', fondo: 'media/025.png' },
-    { pokemon: 'media/074.png', fondo: 'media/074.png' },
-    { pokemon: 'media/077.png', fondo: 'media/077.png' },
-    { pokemon: 'media/079.png', fondo: 'media/079.png' },
-    { pokemon: 'media/104.png', fondo: 'media/104.png' },
-    { pokemon: 'media/107.png', fondo: 'media/107.png' },
-    { pokemon: 'media/143.png', fondo: 'media/143.png' },
-    { pokemon: 'media/397.png', fondo: 'media/397.png' },
-    { pokemon: 'media/415.png', fondo: 'media/415.png' },
-    { pokemon: 'media/613.png', fondo: 'media/613.png' },
-    { pokemon: 'media/643.png', fondo: 'media/643.png' },
-    { pokemon: 'media/778.png', fondo: 'media/778.png' }
+    { pokemon: 'media/001.png', fondo: 'media/001.png', num: 0},
+    { pokemon: 'media/004.png', fondo: 'media/004.png', num: 0},
+    { pokemon: 'media/007.png', fondo: 'media/007.png', num: 0},
+    { pokemon: 'media/025.png', fondo: 'media/025.png', num: 0},
+    { pokemon: 'media/074.png', fondo: 'media/074.png', num: 0},
+    { pokemon: 'media/107.png', fondo: 'media/107.png', num: 0},
+    { pokemon: 'media/397.png', fondo: 'media/397.png', num: 0},
+    { pokemon: 'media/613.png', fondo: 'media/613.png', num: 0},
+    { pokemon: 'media/643.png', fondo: 'media/643.png', num: 0},
 ];
 
+// array para guardar las imagenes que se han usado
+var imagenesUsadas = [];
 
-        
 window.onload = function() {
     // Llama a la función para mostrar imágenes aleatorias al cargar la página
     for (var i = 1; i <= 3; i++) {
@@ -27,6 +22,7 @@ window.onload = function() {
     // Función para obtener una imagen aleatoria del array
     function obtenerImagenAleatoria() {
         var indiceAleatorio = Math.floor(Math.random() * imagenes.length);
+        imagenesUsadas.push(imagenes[indiceAleatorio]);
         return imagenes.splice(indiceAleatorio, 1)[0];
     }
 
@@ -35,13 +31,19 @@ window.onload = function() {
         var imagenAleatoria = document.getElementById('drag' + id);
         var rutaImagen = obtenerImagenAleatoria();
         imagenAleatoria.src = rutaImagen.pokemon;
-    
         // Asocia el fondo correspondiente al bioma de manera aleatoria entre los 3 biomas si no se ha asignado aun
-
         while (true) {
             var biomaAleatorio = Math.floor(Math.random() * 3) + 1;
             var bioma = document.getElementById('bioma' + biomaAleatorio);
             if (bioma.getAttribute('use') == 'False') {
+                // Se cambia la variable de num del vector de imagenes usadas al numero de biomaAleatorio
+                for(var i = 0; i <= imagenesUsadas.length; i++) {
+                    var pokemon = imagenesUsadas[i].pokemon;
+                    if( pokemon == rutaImagen.pokemon) {
+                        imagenesUsadas[i].num = biomaAleatorio;
+                        break
+                    }
+                }
                 // Asigna el fondo al bioma cambiando el style de background-image
                 bioma.style.backgroundImage = 'url(' + rutaImagen.fondo + ')';
                 // la imagen de fondo se ajusta al tamaño del bioma
@@ -50,14 +52,12 @@ window.onload = function() {
                 bioma.style.backgroundPosition = 'center';
                 // ajusta la altura del bioma para que la imagen de fondo se muestre completa
                 bioma.style.height = '15rem';
-
                 bioma.setAttribute('use', 'True');
                 break;
             }
         }
     }
 }
-
 
 // funcion para el arrastre de imagenes asi como soltarlas en el sitio de la drop area
 function allowDrop(ev) {
@@ -71,22 +71,33 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var draggedImage = document.getElementById(data);
+    var src = 'media/' + draggedImage.src.split('/').pop();
+    for(var i = 0; i <= imagenesUsadas.length; i++) {
+        var pokemon = imagenesUsadas[i].pokemon;
+        if( pokemon == src) {
+            var posicioncorrecta = imagenesUsadas[i].num;
+            break
+        }
+    }
+    // obtener el id de la drop area
+    var dropArea = ev.target.id;
+    var biomadrop = 'bioma' + (posicioncorrecta);
+    if (dropArea ==  biomadrop) {
+        ev.target.appendChild(draggedImage);
+        // deshabilita el arrastre de la imagen
+        draggedImage.setAttribute('draggable', 'false');
+    }
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    var draggables = document.querySelectorAll('.drop-area img');
+    var draggables = document.querySelectorAll('.Pokearea img');
     var dropAreas = document.querySelectorAll('.drop-area');
-
     dropAreas.forEach(function (dropArea) {
         dropArea.addEventListener('dragover', allowDrop);
         dropArea.addEventListener('drop', drop);
     });
-
     draggables.forEach(function (draggable) {
         draggable.addEventListener('dragstart', drag);
-    }
-    );
-}
-);
-
+    });
+});
